06fa538f4271110f83568cee09c47848efc35a4a
ryan-hayward <rchayward99@gmail.com>
2023-12-14 14:53:46 -0600

    testing with linked email address

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const app = require('./app')
 
 const port = process.env.PORT
 
+// Adding comments to test git commit on new email address
 app.listen(port, (err) => {
     if (err) console.log(err);
     console.log('Server is up and running on port ' + port)
