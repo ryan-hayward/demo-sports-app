c7a5d1ecc0ca132e2671937c57b6dbdf9ce1f25a
ryan-hayward <rchayward99@gmail.com>
2024-02-19 21:52:33 -0600

    started collecting game metadata

--- data_collection/scripts/get_game_data.py
+++ data_collection/scripts/get_game_data.py
@@ -1,74 +1,121 @@
 from bs4 import BeautifulSoup
-import time, requests, sys
+import time, requests, datetime
 
-# append path to database directory here, as we will be accessing a DB model to write data into
-sys.path.append('./data_collection')
-from db.players.models import Game
-
+# declare Request Counter
+REQUEST_COUNTER = 0
 
 
 
-def get_game_data(game_url: str):
-    # declare game data object
+'''
+Function to be utilized as module; takes a game url and returns a dictionary representing game data
+'''
+def get_game_data(game_url: str) -> dict:
+    # declare empty game data dict
     game = {
-        'gameID',
-        'date',
-        'playoff',
-        'home_team',
-        'away_team',
-        'home_coach',
-        'away_coach',
-        'stadium',
-        'attendance',
-        'h1q_pts',
-        'h2q_pts',
-        'h3q_pts',
-        'h4q_pts',
-        'a1q_pts',
-        'a2q_pts',
-        'a3q_pts',
-        'a4q_pts',
-        'toss_winner',
-        'favored_team',
-        'favored_by',
-        'over_under',
-        'head_ref',
-        'scorigami',
-        'start_time_et',
-        'week',
-        'day_of_week',
-        'rest_days',
-        'miles_traveled',
-        'temp_f',
-        'wind_chill_f',
-        'humidity',
-        'wind_speed',
-        'home_yds',
-        'home_pass_yds',
-        'home_rush_yds',
-        'home_fds',
-        'home_tos',
-        'home_penalties',
-        'home_penalty_yds',
-        'home_third_down_conv',
-        'home_third_down_att',
-        'home_fourth_down_conv',
-        'home_fourth_down_att',
-        'home_top',
-        'away_yds',
-        'away_pass_yds',
-        'away_rush_yds',
-        'away_fds',
-        'away_tos',
-        'away_penalties',
-        'away_penalty_yds',
-        'away_third_down_conv',
-        'away_third_down_att',
-        'away_fourth_down_conv',
-        'away_fourth_down_att',
-        'away_top'
+        'gameID': '',
+        'date': '', #
+        'playoff': '',
+        'home_team': '', #
+        'away_team': '', #
+        'home_coach': '', #
+        'away_coach': '', #
+        'stadium': '',
+        'attendance': '',
+        'h1q_pts': '',
+        'h2q_pts': '',
+        'h3q_pts': '',
+        'h4q_pts': '',
+        'a1q_pts': '',
+        'a2q_pts': '',
+        'a3q_pts': '',
+        'a4q_pts': '',
+        'toss_winner': '',
+        'favored_team': '',
+        'favored_by': '',
+        'over_under': '',
+        'head_ref': '',
+        'scorigami': '',
+        'start_time_et': '', #
+        'week': '',
+        'day_of_week': '', #
+        'rest_days': '',
+        'miles_traveled': '',
+        'temp_f': '',
+        'wind_chill_f': '',
+        'humidity': '',
+        'wind_speed': '',
+        'home_yds': '',
+        'home_pass_yds': '',
+        'home_rush_yds': '',
+        'home_fds': '',
+        'home_tos': '',
+        'home_penalties': '',
+        'home_penalty_yds': '',
+        'home_third_down_conv': '',
+        'home_third_down_att': '',
+        'home_fourth_down_conv': '',
+        'home_fourth_down_att': '',
+        'home_top': '',
+        'away_yds': '',
+        'away_pass_yds': '',
+        'away_rush_yds': '',
+        'away_fds': '',
+        'away_tos': '',
+        'away_penalties': '',
+        'away_penalty_yds': '',
+        'away_third_down_conv': '',
+        'away_third_down_att': '',
+        'away_fourth_down_conv': '',
+        'away_fourth_down_att': '',
+        'away_top': ''
     }
+
+    soup = get_soup(game_url)
+
+    scorebox = soup.find("div", {"class": "scorebox"})
+
+    game = get_scorebox_elems(scorebox, game)
     
+    
+'''
+@TODO come back and write "if" blocks to catch when an element is not present. When not present, populate None
+'''
+def get_scorebox_elems(scorebox, game: dict) -> dict:
+    # get the home team ID
+    teams = scorebox.find_all("a", href=lambda href: href and href.startswith("/teams/"))
+
+    # if exactly two teams cannot be found, raise an exception
+    if len(teams) != 2:
+        raise Exception("Invalid amount of teams found. Expected 2, found %." % len(teams))
+    
+    # trim first 7 characters from href to get team abbreviations
+    game['away_team'] = teams[0]['href'][7:10]
+    game['home_team'] = teams[1]['href'][7:10]
+
+    # get coaches
+    coaches = scorebox.find_all("a", href=lambda href: href and href.startswith("/coaches/"))
+
+    # if exactly two teams cannot be found, raise an exception
+    if len(coaches) != 2:
+        raise Exception("Invalid amount of coaches found. Expected 2, found %." % len(coaches))
+    
+    # trim first 7 characters from href to get team abbreviations
+    game['away_coach'] = coaches[0]['href'].replace('/coaches/', '').replace('.htm', '')
+    game['home_coach'] = coaches[1]['href'].replace('/coaches/', '').replace('.htm', '')
+
+    # get metadata sub div
+    metadata = scorebox.find_all("div", {'class': 'scorebox_meta'})[0]
+    # get date string
+    date_str = metadata.find("div").text
+    # get day of week
+    day_of_week = date_str[0:date_str.find(" ")]
+    game['day_of_week'] = day_of_week.weekday()
+    # get date
+    date = date_str.replace(day_of_week, '').strip()
+    game['date'] = datetime.datetime.strptime(date, '%b %d, %Y').strftime('%Y-%m-%d')
+    
+    # @TODO add start time, attendance, time of game
+
 
 
 '''
