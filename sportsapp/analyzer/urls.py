724bb1bc9b82262602e4b6ef4a3bd3b88e225a73
ryan-hayward <rchayward99@gmail.com>
2024-03-13 18:22:25 -0500

    Added get filters by model api

--- sportsapp/analyzer/urls.py
+++ sportsapp/analyzer/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 
+# @TODO change "analyzer" to include api, since that is what it is
 urlpatterns = [
     # /analyzer/
     path("", views.index, name="index"),
     # /analyzer/linreg
     path("linreg/", views.linear_regression, name="lin_reg"),
     # /analyzer/standard_search
-    path("standard_search/", views.standard_search, name="std_search")
+    path("standard_search/", views.standard_search, name="std_search"),
+    # /analyzer/get_filters
+    path("get_filters/", views.get_filters, name="get_filters")
 ]
--- sportsapp/analyzer/views.py
+++ sportsapp/analyzer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.apps import apps
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -49,8 +50,9 @@ def linear_regression(request):
 
 @api_view(['GET'])
 # standard search
+# @TODO instead of hard-coding Game, make method model agnostic as get_filters is
 def standard_search(request):
-    tgt_model = request.GET.get('type')
+    tgt_model = request.GET.get('search_type')
     # mark invalid request if target model is specified incorrectly
     if tgt_model not in VALID_MODELS:
         return Response('Invalid search type.', status=400)
@@ -59,7 +61,9 @@ def standard_search(request):
     param_list = []
     for k,v in request.GET.items():
         param_list.append([k, v])
+    # ignore param specifying target model
     param_list = param_list[1:]
+    # get target data given target model
     tgt_data = Game.objects.all()
     for param in param_list:
         filter = param[0]
@@ -67,3 +71,24 @@ def standard_search(request):
     # format data for return
     data = list(tgt_data.values())
     return JsonResponse(data, safe=False)
+
+
+@api_view(['GET'])
+# get eligible filters for a given model
+# @TODO split model string to name conversion into a helper method that can be leveraged repeatedly
+def get_filters(request):
+    # try to get the model based on the string passed from client
+    search_type = request.GET.get('search_type')
+    # find the target model
+    models = apps.get_models()
+    tgt_model = None
+    for model in models:
+        if model.__name__ == search_type:
+            tgt_model = model
+            continue
+    if tgt_model == None:
+        return JsonResponse("Invalid model name. Select from dropdown.", safe=False)
+    else:
+        # get potential attrs to filter by from the model
+        attr_list = [a.name for a in tgt_model._meta.get_fields()]
+        return JsonResponse(attr_list, safe=False)
